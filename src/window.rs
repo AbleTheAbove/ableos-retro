ee62a40f67aff6bf6a1cd3e8b62caa4f3313aa62
Able <abl3theabove@gmail.com>
2021-06-02 20:49:24 -0500

    Ahhhh

--- src/window.rs
+++ src/window.rs
@@ -1,6 +1,26 @@
 use vga::colors::Color16;
 use vga::writers::{Graphics640x480x16, GraphicsWriter};
 
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref GRAPHICS: Graphics640x480x16 = {
+        let mode = Graphics640x480x16::new();
+        mode
+    };
+}
+
+pub static WINDOW_BORDER_COLOR: Color16 = Color16::LightBlue;
+pub static WINDOW_DECORATOR_COLOR: Color16 = Color16::LightBlue;
+pub static WINDOW_DECORATOR_TEXT_COLOR: Color16 = Color16::Black;
+/*
+pub struct Size {
+    x: usize,
+    y: usize,
+}
+
+impl core::ops::Add for Size {}
+*/
 pub struct Window<'a> {
     title: &'a str,
     offset: (isize, isize),
@@ -11,32 +31,23 @@ pub fn windows() {
     let window = Window {
         title: "AbleOS Window Example",
         offset: (0, 0),
-        size: (400, 80),
+        size: (400, 60),
     };
 
-    let mode = Graphics640x480x16::new();
-    mode.set_mode();
-    mode.clear_screen(Color16::Black);
+    GRAPHICS.set_mode();
+    GRAPHICS.clear_screen(Color16::Black);
     // Left line
-    mode.draw_line(
+    GRAPHICS.draw_line(
         (0 + window.offset.0, 0 + window.offset.1),
         (
             0 + window.offset.0,
             window.size.1 as isize + window.offset.1,
         ),
-        Color16::White,
+        WINDOW_BORDER_COLOR,
     );
-    // Uppermost line
-    mode.draw_line(
-        (0 + window.offset.0, 0 + window.offset.1),
-        (
-            window.size.0 as isize + window.offset.0,
-            0 + window.offset.1,
-        ),
-        Color16::White,
-    );
+
     // Lowest line
-    mode.draw_line(
+    GRAPHICS.draw_line(
         (
             0 + window.offset.0,
             window.size.1 as isize + window.offset.1,
@@ -45,49 +56,38 @@ pub fn windows() {
             window.size.0 as isize + window.offset.0,
             window.size.1 as isize + window.offset.1,
         ),
-        Color16::White,
+        WINDOW_BORDER_COLOR,
     );
 
     //right most line
-    mode.draw_line(
+    GRAPHICS.draw_line(
         (
             window.size.0 as isize + window.offset.0,
             window.size.1 as isize + window.offset.1,
         ),
         (window.size.0 as isize + window.offset.0, window.offset.1),
-        Color16::White,
-    );
-    // Second top line
-    mode.draw_line(
-        (0 + window.offset.0, 30 + window.offset.1),
-        (
-            window.size.0 as isize + window.offset.0,
-            30 + window.offset.1,
-        ),
-        Color16::White,
+        WINDOW_BORDER_COLOR,
     );
 
-    {
-        // A simple colored window display that I think should be fully implemented
-        for y in 1..30 {
-            for x in 1..window.size.0 {
-                mode.set_pixel(
-                    x + window.offset.0 as usize,
-                    y + window.offset.1 as usize,
-                    Color16::Green,
-                );
-            }
+    // A simple window decorator that I think should be fully implemented
+    for y in 0..20 {
+        for x in 1..window.size.0 {
+            GRAPHICS.set_pixel(
+                x + window.offset.0 as usize,
+                y + window.offset.1 as usize,
+                WINDOW_DECORATOR_COLOR,
+            );
         }
     }
 
     let title_width = window.title.len() * 8;
     for (offset, character) in window.title.chars().enumerate() {
-        mode.draw_character(
+        GRAPHICS.draw_character(
             // TODO: Get length of character size and then do math
             (window.offset.0 as usize + ((window.size.0 - title_width) / 2)) as usize + offset * 8,
-            (12 + window.offset.1) as usize,
+            (6 + window.offset.1) as usize,
             character,
-            Color16::White,
+            WINDOW_DECORATOR_TEXT_COLOR,
         )
     }
 }
