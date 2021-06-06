8a238e37bef17386901ab0400a77c4e1673eb639
Able <abl3theabove@gmail.com>
2021-06-06 03:14:56 -0500

    AFS work begin

---
+++ src/sri/fs/afs/mod.rs
@@ -0,0 +1,17 @@
+//! The Able File System will be the system wide file system
+
+struct File {
+	owner_id: u8,
+}
+
+//    Create a file
+//    Delete a file
+//    Open a file
+//    Close a file
+//    Read data from a file
+//    Write data to a file
+//    Reposition the current file pointer in a file
+//    Append data to the end of a file
+//    Truncate a file (delete its contents)
+//    Rename a file
+//    Copy a file
