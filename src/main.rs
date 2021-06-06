142f964be8d59870a0858ec7e3fbdf19bb82d6ba
anyusernameworks <patlymlatun@gmail.com>
2021-06-06 14:09:03 -0700

    Fix duplicate

--- src/main.rs
+++ src/main.rs
@@ -121,45 +121,6 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 		}
 	}
 
-	use alloc::format;
-	let v_str = format!("{}", kernel_state::KERNEL_STATE.lock().version);
-	println(&v_str, (0, 0));
-
-	#[cfg(test)]
-	test_main();
-	use cpuio::outw;
-	unsafe {
-		outw(0x604, 0x2000);
-	}
-
-	// reason for without_interrupts: mouse interrupt handler and init_mouse acquires the same mutex
-	x86_64::instructions::interrupts::without_interrupts(|| {
-		drivers::mouse::init_mouse();
-	});
-
-	fn println(yes: &str, coordinates: (usize, usize)) {
-		let mut offset = 0;
-		let mut offset_y = 0;
-
-		for x in yes.chars() {
-			match x {
-				'\n' => {
-					offset = 0;
-					offset_y += 1;
-				}
-				_ => {
-					GRAPHICS.draw_character(
-						offset * 8 + coordinates.0,
-						offset_y * 8,
-						x,
-						Color16::White,
-					);
-					offset += 1;
-				}
-			}
-		}
-	}
-
 	use task::{executor::Executor, keyboard, Task};
 	let mut executor = Executor::new();
 	executor.spawn(Task::new(example_task()));
