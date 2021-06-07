a6e44293e46959ede024445e269e731b329cc708
Able <abl3theabove@gmail.com>
2021-06-07 02:19:59 -0500

    Shell Changes

--- rash/src/lib.rs
+++ rash/src/lib.rs
@@ -1,13 +1,16 @@
 #![no_std]
 #![warn(missing_docs)]
 
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
 /// The internal data of the shell
 struct Shell {}
 
 /// The publically avalible shell runner
 pub fn shell() {
-	/// Represent the shell data in an accessable way
+	// Represent the shell data in an accessable way
 	let mut shell = Shell {};
-	/// the Prompt
+	// the Prompt
+	// TODO: allow this to be configured
 	let prompt = "~>";
 }
