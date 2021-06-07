38022bcb7925179e9208651c6a66f6f2d0a01807
anyusernameworks <patlymlatun@gmail.com>
2021-06-06 20:56:53 -0700

    Merge.

--- src/interrupts/apic.rs
+++ src/interrupts/apic.rs
@@ -1,11 +1,17 @@
-use crate::kernel_state::cpuid::{EDXFeatureMasks};
+use crate::kernel_state::cpuid::EDXFeatureMasks;
 use raw_cpuid::{cpuid, CpuIdResult};
+use x86::msr::{rdmsr};
 
-/// Returns whether or not apic is available on this processor.
+/// Returns true if apic is available on this processor.
 /// Sets register eax to 1, then queries cpuid
 pub fn has_apic() -> bool {
 	// Note: this is not where it sets eax to 1.
 	// This is.
-	let result = cpuid!(1,0);
+	let result = cpuid!(1, 0);
 	(result.edx & EDXFeatureMasks::APIC as u32) > 0
 }
+
+const IA32_APIC_BASE_MSR: u32 = 0x1B;
+const IA32_APIC_BASE_MSR_BSP: u32 = 0x100; // Processor is a BSP
+const IA32_APIC_BASE_MSR_ENABLE: u32 = 0x800;
+
